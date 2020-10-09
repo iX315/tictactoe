0ffdca65e9268060f0c721798f7feff5658f96a7
Jury Giannelli <iX315@me.com>
2020-10-09 19:53:20 +0200

    Updates

---
+++ precache-manifest.23299f28101bc89822501f8e1d2465d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc23bcbfb14ec7d14962907454b926b",
+    "url": "/ticktactoe/index.html"
+  },
+  {
+    "revision": "2488e37f7808c554af06",
+    "url": "/ticktactoe/static/css/main.020377e9.chunk.css"
+  },
+  {
+    "revision": "a6b8b4f4bf4fb850ff2b",
+    "url": "/ticktactoe/static/js/2.757f6bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticktactoe/static/js/2.757f6bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2488e37f7808c554af06",
+    "url": "/ticktactoe/static/js/main.c312df7f.chunk.js"
+  },
+  {
+    "revision": "8261267d08153b0da34d",
+    "url": "/ticktactoe/static/js/runtime-main.488a3090.js"
+  },
+  {
+    "revision": "a793a851ef57fdf3f346a36b239608ed",
+    "url": "/ticktactoe/static/media/circle.a793a851.svg"
+  },
+  {
+    "revision": "318c456a378e83328c603036d16ca3fb",
+    "url": "/ticktactoe/static/media/cross.318c456a.svg"
+  }
+]);
