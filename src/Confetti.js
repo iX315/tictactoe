ebc14b20433362e5fa877c37988cc283fcb69a16
Jury Giannelli <iX315@me.com>
2020-10-09 21:09:01 +0200

    confetti settings

--- src/Confetti.js
+++ src/Confetti.js
@@ -4,5 +4,11 @@ import Confetti from "react-confetti";
 
 export default () => {
 	const { width, height } = useWindowSize();
-	return <Confetti width={width} height={height} />;
+    return <Confetti
+        width={width}
+        height={height}
+        numberOfPieces={50}
+        confettiSource={{ x: width/2, y: height/2 }}
+        recycle={false}
+    />;
 };
